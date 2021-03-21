c441f97164842302e2824c4ca9abd05189dd1ca7
islander <miku.miku.miku@sjtu.edu.cn>
2021-03-21 19:48:00 +0800

    feat(rename): do not need \ in your argument

--- rename.py
+++ rename.py
@@ -6,24 +6,48 @@ import argparse
 
 def rename(fd, template):
     for fn in os.listdir(fd):
-        in_fp = osp.join(fd, fn)
-        match = re.match(template, fn)
+        
+        try:
+            fn_content, fn_ext = fn.rsplit('.', 1)
+        except ValueError as e:
+            continue
+
+        match = re.match(template, fn_content)
 
         if match:
             num = match.group(1)
-            ext = match.group(2)
-            out_fn = f'{num}.{ext}'
+            out_fn = f'{num}.{fn_ext}'
             out_fp = osp.join(fd, out_fn)
+            in_fp = osp.join(fd, fn)
             os.rename(in_fp, out_fp)
 
 
+class Holder(object):
+    def __init__(self, content=None):
+        self.content = content
+
+
 if __name__ == '__main__':
+    number = 'num5'
+
     parser = argparse.ArgumentParser('rename files in directory')
     parser.add_argument(
         '-tmp', '--template', type = str, 
-        help = 'file name template, in regex format, () appears first in number, second in extension, '
-               'e.g., Shingeki no Kyojin - ([0-9]+) \[BDRip 1280x720\]-muxed\.(mp4)')
+        help = f'数字部分用 {number} 指代，不写扩展名'
+    )
     args = parser.parse_args()
-    print(f'renaming with template: {args.template}')
-    template = re.compile(args.template)
+    template = Holder(args.template)
+
+    # 修改正则化特殊字符
+    def _replace(*symbols):
+        for symbol in symbols:
+            template.content = template.content.replace(symbol, '\\' + symbol)
+
+    _replace('(', ')', '{', '}', '[', ']', '.', '*', '^', '$', '&', '?')
+    template = template.content
+
+    template = template.replace(number, '(\d+)')
+
+    print(f'renaming with template: {template}')
+    template = re.compile(template)
     rename('.', template)
